df94a0bcd9f2ec81907d694d5ccfff326ef79c0a
Puneet Kollipara <45177858+pyneet@users.noreply.github.com>
2025-04-23 10:24:54 -0400

    Update run.sh with corrected DC urban tree canopy GeoJSON URL

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl "https://opendata.dc.gov/api/download/v1/items/cd2da0109c1b46c3a2ec9890e44623d3/geojson?layers=1" --output "data.geojson"
+curl "https://hub.arcgis.com/api/v3/datasets/cd2da0109c1b46c3a2ec9890e44623d3_1/downloads/data?format=geojson&spatialRefId=4326&where=1%3D1" --output "data.geojson"
