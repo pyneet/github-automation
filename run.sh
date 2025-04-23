74d633f8993f91da7900453b369d3e9deebb478f
Puneet Kollipara <45177858+pyneet@users.noreply.github.com>
2025-04-23 10:23:00 -0400

    Update run.sh with urban tree canopy GeoJSON 2

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl "hhttps://opendata.dc.gov/api/download/v1/items/cd2da0109c1b46c3a2ec9890e44623d3/geojson?layers=1" --output "data.geojson"
+curl "https://opendata.dc.gov/api/download/v1/items/cd2da0109c1b46c3a2ec9890e44623d3/geojson?layers=1" --output "data.geojson"
