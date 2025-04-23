551b1a84b8a26ba7424b627517d9cdd69b879610
Puneet Kollipara <45177858+pyneet@users.noreply.github.com>
2025-04-23 10:21:56 -0400

    Update run.sh with urban tree canopy GeoJSON

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl "https://davidmegginson.github.io/ourairports-data/airports.csv" --output "data_airports.csv"
+curl "hhttps://opendata.dc.gov/api/download/v1/items/cd2da0109c1b46c3a2ec9890e44623d3/geojson?layers=1" --output "data.geojson"
