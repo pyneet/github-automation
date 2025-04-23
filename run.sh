7a9ba7b85b9a94e9146f21495b72974f8fd288ce
Puneet Kollipara <45177858+pyneet@users.noreply.github.com>
2025-04-23 10:03:14 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl "https://hub.arcgis.com/api/download/v1/items/cd2da0109c1b46c3a2ec9890e44623d3/csv?redirect=false&layers=1" --output data.csv
+curl "https://hub.arcgis.com/api/download/v1/items/cd2da0109c1b46c3a2ec9890e44623d3/csv?redirect=true&layers=1" --output data.csv
