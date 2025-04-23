99a3259cbf8df3c8a2349c28310f2773f89d7610
Puneet Kollipara <45177858+pyneet@users.noreply.github.com>
2025-04-23 10:12:04 -0400

    Update run.sh with airports dataset

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl "https://hub.arcgis.com/api/v3/datasets/cd2da0109c1b46c3a2ec9890e44623d3_1/downloads/data?format=csv&spatialRefId=26985&where=1%3D1" --output data.csv
+curl "https://davidmegginson.github.io/ourairports-data/airports.csv" --output "data_airports.csv"
